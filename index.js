17e0699e98676b7867914f40aaee3713a31c9b1c
Dhanuska Madhushan <gamagemadhushan@gmail.com>
2022-10-22 13:51:41 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded',()=>{
+    const grid = document.querySelector('.grid');
+    const doodler = document.createElement('div');
+    let doodlerLeftSpace = 50;
+    let doodlerBottomSpace = 200;
+
+    function createDoodler() {
+        grid.appendChild(doodler);
+        doodler.classList.add('doodler');
+        doodler.style.left = doodlerLeftSpace + 'px';
+        doodler.style.bottom = doodlerBottomSpace + 'px';
+
+    }
+
+    createDoodler();
+
+})
