0ee5f6b3a74d099200ddae2bd7e8b545254cfcf7
Dhanuska Madhushan <gamagemadhushan@gmail.com>
2022-10-22 17:24:36 +0530

    Create a doodle and platforms

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ document.addEventListener('DOMContentLoaded',()=>{
     const grid = document.querySelector('.grid');
     const doodler = document.createElement('div');
     let doodlerLeftSpace = 50;
-    let doodlerBottomSpace = 200;
+    let doodlerBottomSpace = 150;
+    let isGameOver =false;
+    let platformCount = 5 ;
 
     function createDoodler() {
         grid.appendChild(doodler);
@@ -12,6 +14,55 @@ document.addEventListener('DOMContentLoaded',()=>{
 
     }
 
-    createDoodler();
+//    class Platform{
+//     constructor(newPlatBottom){
+//         // this.bottom = newPlatBottom;
+//         // this.left = Math.random()*315;
+//         this.visual =document.createElement('div');
 
+//         const visual = this.visual;
+//         visual.classList.add('platform');
+//         this.style.left = Math.random()*315 + 'px';
+//         this.style.bottom = newPlatBottom + 'px';
+//         grid.appendChild(visual);
+//         }
+
+//    }
+
+   class Platform {
+    constructor(newPlatBottom) {
+      this.left = Math.random() * 315;
+      this.bottom = newPlatBottom;
+      this.visual = document.createElement('div');
+
+      const visual = this.visual;
+      visual.classList.add('platform');
+      visual.style.left = this.left + 'px';
+      visual.style.bottom = this.bottom + 'px';
+      grid.appendChild(visual);
+    }
+  }
+  
+
+    function createPlatforms(){
+        for (let i =0;i<platformCount ;i++){
+            let platformGap = 600/platformCount;
+           
+            let newPlatBottom = 100 + i*platformGap;
+            console.log(newPlatBottom);
+
+             new Platform(newPlatBottom);
+
+        }
+    }
+
+    function start(){
+        if (!isGameOver ){
+            createDoodler();
+            createPlatforms();
+        }
+    }
+    
+    //attach button
+    start();
 })
