e245c2c3c7ca23a37dfd573789dd968f45fca651
Dhanuska Madhushan <gamagemadhushan@gmail.com>
2022-10-23 20:51:23 +0530

    Finalized first stage of the game

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ document.addEventListener('DOMContentLoaded',()=>{
     let isGoingLeft = false;
     let leftTimerId ;
     let rightTimerId ;
+    let score = 0;
 
     
 
@@ -61,6 +62,15 @@ document.addEventListener('DOMContentLoaded',()=>{
                 platform.bottom -=4;
                 let visual = platform.visual;
                 visual.style.bottom = platform.bottom +'px';
+                if(platform.bottom < 10){
+                    let firstPlatform = platforms[0].visual;
+                    firstPlatform.classList.remove('platform');
+                    platforms.shift();
+                    score++;
+                    let newPlatform = new Platform(600);
+                    platforms.push(newPlatform);
+
+                }
 
             })
         }
@@ -105,8 +115,14 @@ document.addEventListener('DOMContentLoaded',()=>{
     function gameOver(){
         console.log('Game Over');
         isGameOver = true;
+        while(grid.firstChild){
+            grid.removeChild(grid.firstChild);
+        }
+        grid.innerHTML = score;
         clearInterval(upTimerId);
         clearInterval(downTimerId);
+        clearInterval(leftTimerId);
+        clearInterval(rightTimerId);
     }
 
     function control(e){
@@ -171,6 +187,7 @@ document.addEventListener('DOMContentLoaded',()=>{
             setInterval(movePlatforms,30);
             jump();
             document.addEventListener('keyup',control);
+
         }
     }
 
